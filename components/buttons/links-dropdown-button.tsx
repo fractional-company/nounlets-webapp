4e6e5ea798ab6aeae081f0a547077010430048b4
cvetlicnifractional <86823960+cvetlicnifractional@users.noreply.github.com>
2022-09-02 16:27:15 +0200

    fix links sizing

--- components/buttons/links-dropdown-button.tsx
+++ components/buttons/links-dropdown-button.tsx
@@ -35,7 +35,7 @@ export default function LinksDropdownButton(): JSX.Element {
             <div className="divide-y divide-gray-1">
               <Menu.Item>
                 <Button className="basic w-full space-x-2 !justify-start !rounded-none">
-                  <a href="https://medium.com/@deeze/b76bbb4e42cc" target="_blank" rel="noreferrer" className="space-x-2 w-full flex">
+                  <a href="https://medium.com/@deeze/b76bbb4e42cc" target="_blank" rel="noreferrer" className="space-x-2 w-full h-full items-center flex">
                     <IconMedium className="h-[16px] w-auto" />
                     <span>Nounlets Explained</span>
                   </a>
@@ -43,7 +43,7 @@ export default function LinksDropdownButton(): JSX.Element {
               </Menu.Item>
               <Menu.Item>
                 <Button className="basic w-full space-x-2 !justify-start !rounded-none">
-                  <a href="https://nouns.wtf" target="_blank" rel="noreferrer" className="space-x-2 w-full flex">
+                  <a href="https://nouns.wtf" target="_blank" rel="noreferrer" className="space-x-2 w-full h-full items-center flex">
                     <IconHeartHollow className="h-[16px] w-auto" />
                     <span>Nouns.wtf</span>
                   </a>
@@ -51,7 +51,7 @@ export default function LinksDropdownButton(): JSX.Element {
               </Menu.Item>
               <Menu.Item>
                 <Button className="basic w-full space-x-2 !justify-start !rounded-none">
-                  <a href="https://twitter.com/tessera" target="_blank" rel="noreferrer" className="space-x-2 w-full flex">
+                  <a href="https://twitter.com/tessera" target="_blank" rel="noreferrer" className="space-x-2 w-full h-full items-center flex">
                     <IconTwitter className="h-[16px] w-auto" />
                     <span>Twitter</span>
                   </a>
@@ -59,7 +59,7 @@ export default function LinksDropdownButton(): JSX.Element {
               </Menu.Item>
               <Menu.Item>
                 <Button className="basic w-full space-x-2 !justify-start !rounded-none">
-                  <a href={buildEtherscanAddressLink(process.env.NEXT_PUBLIC_NOUN_VAULT_ADDRESS!)} target="_blank" rel="noreferrer" className="space-x-2 w-full flex">
+                  <a href={buildEtherscanAddressLink(process.env.NEXT_PUBLIC_NOUN_VAULT_ADDRESS!)} target="_blank" rel="noreferrer" className="space-x-2 w-full h-full items-center flex">
                     <IconEtherscan className="h-[16px] w-auto" />
                     <span>Etherscan</span>
                   </a>
@@ -67,7 +67,7 @@ export default function LinksDropdownButton(): JSX.Element {
               </Menu.Item>
               <Menu.Item>
                 <Button className="basic w-full !justify-start !rounded-none">
-                  <a href="https://tessera.co/" target="_blank" rel="noreferrer" className="space-x-2 w-full flex">
+                  <a href="https://tessera.co/" target="_blank" rel="noreferrer" className="space-x-2 w-full h-full items-center flex">
                     <IconFractionalLogo className="h-[16px] w-auto text-secondary-green" />
                     <span>Tessera</span>
                   </a>
